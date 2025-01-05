9e7d599e8543398f75075a802e0de2714254c5d2
Duncan Hill <github.commits@cricalix.net>
2025-01-05 14:54:12 +0000

    Enable transcode disabling in advanced settings

--- ct/jellyfin.sh
+++ ct/jellyfin.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
+source ../misc/build.func
 # Copyright (c) 2021-2025 tteck
 # Author: tteck (tteckster)
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
@@ -7,6 +7,8 @@ source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVE/m
 
 # App Default Values
 APP="Jellyfin"
+var_can_use_transcode=1
+var_enable_transcode=1
 var_tags="media"
 var_cpu="2"
 var_ram="2048"
